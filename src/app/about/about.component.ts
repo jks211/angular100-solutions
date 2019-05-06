313a664a675ccadf68f09674f4c4ee4d6cc9c68b
Judy Lipinski <judy@karmoxie.com>
2019-05-06 01:18:21 -0400

    fixed formatting for home and navbar

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,23 +3,31 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-about",
   template: `
-    <div class="jumbotron text-center">
-      <h1 class="display-4">Welcome to {{ title }}!</h1>
-      <button
+
+
+    <div class="container">
+  <div class="jumbotron my-5 mx-5 ">
+    <h1 class="display-4">Welcome to {{ title }}!</h1>
+    <p class="lead">View and Add albums
+    </p>
+    <button
         type="button"
         routerLink="/albums"
         class="btn btn-primary text-center mt-3 mx-auto"
       >
         View Albums
       </button>
-    </div>
+  </div>
+</div>
+
+
   `,
   styles: []
 })
 export class AboutComponent implements OnInit {
   title = "My Angular Albums";
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class NavbarComponent implements OnInit {
   title = "My Albums Project";
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
