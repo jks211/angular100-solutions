81408bcc0f0ecf5cecc51f2d71f40955222fa8ac
Judy Lipinski <judy@karmoxie.com>
2019-09-28 13:52:41 -0500

    updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,24 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-about",
   template: `
-
-
-    <div class="container">
-  <div class="jumbotron my-5 mx-5 ">
-    <h1 class="display-4">Welcome to {{ title }}!</h1>
-    <p class="lead">View and Add albums
-    </p>
-    <button
-        type="button"
-        routerLink="/albums"
-        class="btn btn-primary text-center mt-3 mx-auto"
-      >
-        View Albums
-      </button>
-  </div>
-</div>
-
-
+    <>This app is written in ANgular and displays albums</p>
   `,
   styles: []
 })
