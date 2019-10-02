6ca6fc7df0bc69e31684b5a12dca1f38d1dc0ce5
Judy Lipinski <judy@karmoxie.com>
2019-10-02 16:07:30 -0400

    added attribute to selector

--- src/app/albums/album-card/album-card.component.ts
+++ src/app/albums/album-card/album-card.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Album } from "../album.model";
 
 @Component({
-  selector: "app-album-card",
+  selector: "app-album-card[album]",
   templateUrl: "./album-card.component.html",
   styleUrls: ["./album-card.component.css"]
 })
@@ -12,5 +12,9 @@ export class AlbumCardComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.album == undefined) {
+      throw Error("Alum was not passed")
+    }
+  }
 }
