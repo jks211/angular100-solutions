2b018bc7559f430d30c7f69a07ff2b5d8f8fea1a
Judy Lipinski <judy@karmoxie.com>
2019-10-04 06:23:00 -0400

    matched to 1

---
+++ src/app/albums/album-edit/album-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-album-edit',
+  templateUrl: './album-edit.component.html',
+  styleUrls: ['./album-edit.component.css']
+})
+export class AlbumEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
